6f49dd52b066c6549e04ebcab9a858f8b6fa0b2f
alexander <ag.saldivar@gmail.com>
2017-11-10 09:54:37 -0600

    photo upload png

--- src/components/PhotoUpload/upload.js
+++ src/components/PhotoUpload/upload.js
@@ -34,19 +34,6 @@ function imgReduce(image) {
         canvas.height / 2
     );
 
-    var imgData=ctx.getImageData(0,0,canvas.width,canvas.height);
-    var data=imgData.data;
-    for(var i=0;i<data.length;i+=4){
-        if(data[i+3]<255){
-            data[i]=255;
-            data[i+1]=255;
-            data[i+2]=255;
-            data[i+3]=255;
-        }
-    }
-    ctx.putImageData(imgData,0,0);
-
-    console.log(canvas.toDataURL("image/jpeg", 0.5))
-    return canvas.toDataURL("image/jpeg", 0.5)
+    return canvas.toDataURL("image/png")
 }
 export { upload }
