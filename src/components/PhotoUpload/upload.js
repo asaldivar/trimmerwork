7c3a6588a3e28770a8073ef8c194ed5002c3b9a3
alexander <ag.saldivar@gmail.com>
2017-11-10 09:35:46 -0600

    reduce image url

--- src/components/PhotoUpload/upload.js
+++ src/components/PhotoUpload/upload.js
@@ -7,15 +7,28 @@ function upload(formData) {
         const fReader = new FileReader()
 
         fReader.onload = () => {
-            const imageURL = {
-                originalName: photo.name,
-                url: fReader.result
+            const img = document.createElement('img')
+            img.src = fReader.result
+            img.onload = function(){
+                const imageURL = {
+                    originalName: photo.name,
+                    url: imgReduce(img)
+                }
+                resolve(imageURL)
             }
-            resolve(imageURL)
         }
         fReader.readAsDataURL(photo)
     })
 
 }
 
+function imgReduce(img) {
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    ctx.scale(0.5, 0.5);
+    ctx.drawImage(img, 0, 0);
+    return canvas.toDataURL("image/png", 0.5)
+}
 export { upload }
