34adfe4e3bfe1be87322a83ed759fdf476386052
alexander <ag.saldivar@gmail.com>
2017-11-11 18:01:08 -0600

    add middleware redirect http->https

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,20 @@
-require('newrelic');
-var history = require('connect-history-api-fallback');
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(history());
-app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+require('newrelic')
+var history = require('connect-history-api-fallback')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+app.use(history())
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+
+app.use(function (req, res, next) {
+  if (!/https/.test(req.protocol)) {
+		res.redirect("https://" + req.headers.host + req.url)
+  } else {
+		return next()
+  }
+})
+
+app.listen(port)
+console.log('server started '+ port)
