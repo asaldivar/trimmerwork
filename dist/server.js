f5d9e9599829323bd7dba9ab3f74bea8de6ebb14
alexander <ag.saldivar@gmail.com>
2017-11-11 18:07:19 -0600

    prod: redirect http->https

--- dist/server.js
+++ dist/server.js
@@ -4,9 +4,6 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
-app.use(history())
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
 
 app.use(function (req, res, next) {
   if (!/https/.test(req.protocol)) {
@@ -15,6 +12,11 @@ app.use(function (req, res, next) {
 		return next()
   }
 })
+
+app.use(history())
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+
 
 app.listen(port)
 console.log('server started '+ port)
