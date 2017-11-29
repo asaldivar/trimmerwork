1022a3aeeecc06f0143dcb9dc8933a079f72a56a
alexander <ag.saldivar@gmail.com>
2017-11-29 13:51:14 -0800

    revert http to https redirect test

--- dist/server.js
+++ dist/server.js
@@ -1,23 +1,20 @@
-// require('newrelic')
+require('newrelic')
 var history = require('connect-history-api-fallback')
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-var enforce = require('express-sslify')
 app = express()
 
-app.use(function (req, res, next) {
-  if (!/https/.test(req.protocol)) {
-		return res.redirect("https://" + req.headers.host + req.url)
-  } else {
-		return next()
-  }
-})
+// app.use(function (req, res, next) {
+//   if (!/https/.test(req.protocol)) {
+// 		return res.redirect("https://" + req.headers.host + req.url)
+//   } else {
+// 		return next()
+//   }
+// })
 
 app.use(history())
-// app.use(serveStatic(__dirname))
-// app.use(enforce.HTTPS())
-app.use(express.static(__dirname))
+app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 
 
